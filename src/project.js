427215dd9956b886cb943b59eb222d6932e9c4c8
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2025-02-22 17:16:06 -0800

    Make to-do module and start project module.

---
+++ src/project.js
@@ -0,0 +1,5 @@
+import ToDo from './todo';
+
+export default const Project = () =>{
+    let list = [];
+};
---
+++ src/todo.js
@@ -0,0 +1,18 @@
+export default class ToDo {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    changePriority(newPriority){
+        if(newPriority != 'urgent' && newPriority != 'moderate'
+            && newPriority != 'unrushed') console.log('Error');
+        else this.priority = newPriority;
+    }
+
+    getPriority(){
+        return this.priority;
+    }
+}
