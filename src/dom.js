4da15378d2310b27d274c2515cb3cf1b40eab461
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2025-03-06 19:25:33 -0800

    Add a basic checkbox (focusing right now on just getting the format adjusted for a checkbox before moving to fancier checkbox).

--- src/dom.js
+++ src/dom.js
@@ -11,19 +11,22 @@ export default function DOM(project){
             let title = document.createElement('h1');
             let desc = document.createElement('p');
             let leftContainer = document.createElement('div');
+            let upperContainer = document.createElement('div');
 
             li.classList.add('todo');
             date.classList.add('date');
             title.classList.add('title');
             desc.classList.add('desc');
             leftContainer.classList.add('left-container');
+            upperContainer.classList.add('upper-container');
 
             date.innerHTML = toDo.getDate();
             title.innerHTML = toDo.getTitle();
             desc.innerHTML = toDo.getDescription();
             
-            leftContainer.appendChild(title);
-            leftContainer.appendChild(desc);
+            let checkbox = makeCheckbox();
+            upperContainer.append(checkbox, title);
+            leftContainer.append(upperContainer, desc);
             
             li.appendChild(leftContainer);
             li.appendChild(date);
@@ -32,6 +35,29 @@ export default function DOM(project){
             
             ul.appendChild(li);
         }
+    };
+
+    const makeCheckbox = () =>{
+        let checkboxWrapper = document.createElement('div');
+        let checkbox = document.createElement('input');
+        let svg = document.createElement('svg');
+        let circle = document.createElement('circle');
+        let polyline = document.createElement('polyline');
+
+        checkboxWrapper.classList.add('checkbox-wrapper-31');
+        circle.classList.add('background');
+        polyline.classList.add('check');
+        
+        checkbox.type = 'checkbox';
+
+        svg.setAttribute('viewBox', '0 0 35.6 35.6');
+        circle.setAttribute('cx', '17.8');
+        circle.setAttribute('cy', '17.8');
+        circle.setAttribute('r', '14.37');
+        polyline.setAttribute('points', '11.78 18.12 15.55 22.23 25.17 12.87');
+
+        checkboxWrapper.append(checkbox, svg, circle, polyline);
+        return checkboxWrapper;
     };
 
     const setPriority = (toDo, li) =>{
